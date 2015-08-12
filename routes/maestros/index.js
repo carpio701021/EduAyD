01e319fdf64ba53db26789c5264a9b396b6b91e2
carpio701021 <carpio701021@gmail.com>
2015-08-12 17:06:33 -0600

    Creación del proyecto
    Creación del proyecto

---
+++ routes/maestros/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('maestros/index', { nombre_usuario: 'Aqui el nombre usuario' });
+	
+});
+
+/* GET Asignacion de tareas */
+router.get('/asignar_tarea/', function(req, res, next) {
+	res.render('maestros/asignar_tarea', { nombre_usuario: 'Aqui el nombre usuario' });
+});
+
+module.exports = router;
