204e139a84df87111ff74375ad259ebe9151b5a0
jodaches <jodaches@gmail.com>
2015-09-02 12:00:04 -0600

    interfaz ingresar notas (chinin)
    se chingo mi rama

--- routes/maestros/index.js
+++ routes/maestros/index.js
@@ -24,7 +24,6 @@ router.get('/asignar_tarea/', function(req, res, next) {
 
 	var recursos_asigna_tarea = function(tipo_actividad){
 
-
 		var recursos_asigna_tarea1 = function(cursos_del_maestro){
 			res.render('maestros/asignar_tarea', { 
 				nombre_usuario: 'Luis Eduardo' , 
@@ -66,9 +65,38 @@ router.post('/asignar_tarea/subir_archivo/', upload.single('archivo_tarea') , fu
 
 /* GET ingresar notas */
 router.get('/ingresar_notas/', function(req, res, next) {
-	res.render('maestros/ingresar_notas', { nombre_usuario: 'Aqui el nombre usuario' });	
+
+	var recursos_ingresar_notas= function(cursos_del_maestro){
+		res.render('maestros/ingresar_notas', { 
+			nombre_usuario: 'Luis Eduardo' ,
+			cursos: cursos_del_maestro
+		});	
+	}
+
+	var dbconnection = require('../../routes/dbconnection.js');
+    var str_query = 'select c.id_curso,c.nombre_curso from Maestro_x_curso,Curso c where MAESTRO_id_maestro = 1 and c.id_curso = CURSO_id_curso;';	
+
+	dbconnection.exe_query(
+			str_query, 
+			recursos_ingresar_notas,
+			res);
 });
 
+router.post('/ingresar_notas/cargar_tabla/', function(req, res, next) {
+	console.log("post cargar tabla");
+	var recursos_ingresar_notas= function(cursos_del_maestro){
+		console.log("Consulta: " + cursos_del_maestro);
+		res.send(cursos_del_maestro);
+	}
+
+	var dbconnection = require('../../routes/dbconnection.js');
+    var str_query = 'select c.id_curso,c.nombre_curso from Maestro_x_curso,Curso c where MAESTRO_id_maestro = 1 and c.id_curso = CURSO_id_curso;';	
+
+	dbconnection.exe_query(
+			str_query, 
+			recursos_ingresar_notas,
+			res);
+});
 
 
 
