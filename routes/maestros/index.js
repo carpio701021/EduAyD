cf733fd39c79971fd9c9697fddae41b022f28a17
carpio701021 <carpio701021@gmail.com>
2015-09-11 23:28:23 -0600

    Metodo sumar notas

--- routes/maestros/index.js
+++ routes/maestros/index.js
@@ -33,7 +33,7 @@ router.get('/asignar_tarea/', function(req, res, next) {
 
 		//llamada al objeto base de datos
 		var dbconnection = require('../../routes/dbconnection.js');
-		var str_query = 'CALL seccionCursoGrado('+1+');';
+		var str_query = 'CALL seccionCursoGrado('+2+');';
 		//(  myquery , callback_to_query_parameters , callback_to_query , if_error , res){
 		dbconnection.exe_query(
 			str_query, 
